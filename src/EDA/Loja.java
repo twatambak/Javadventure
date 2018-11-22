f9566d4d940d0da0fcd57f4246eaed05a85ac5e7
MatheusBencke <mathbencke@gmail.com>
2018-11-22 15:45:44 -0200

    Começei a lojinha

--- src/EDA/Loja.java
+++ src/EDA/Loja.java
@@ -1,9 +1,22 @@
 package EDA;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  *
  * @author Mathes Bencke e Thiago Luis Watambak
  */
-public class Loja extends Equipamento{
-    private int valor;
-    private int i;
+public class Loja{
+    private int valor = 0;
+    private String nomeItem;
+    private int atributos; 
+    ArrayList<Equipamento> arrayLoja = new ArrayList<>();
+    
+    public Iterator getIteratorLoja(){
+        Iterator itLoja = arrayLoja.iterator();
+        return itLoja;
+    }
+    
+    
 }
