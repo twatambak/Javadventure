86de7032e7d7bcb6232b821872b9eb19b0f98819
MatheusBencke <mathbencke@gmail.com>
2018-11-22 21:19:16 -0200

    Mah oe

--- src/EDA/Loja.java
+++ src/EDA/Loja.java
@@ -8,9 +8,6 @@ import java.util.Iterator;
  * @author Mathes Bencke e Thiago Luis Watambak
  */
 public class Loja{
-    private int valor = 0;
-    private String nomeItem;
-    private int atributos; 
     ArrayList<Equipamento> arrayLoja = new ArrayList<>();
     
     public Iterator getIteratorLoja(){
